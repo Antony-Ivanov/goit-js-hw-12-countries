a7f4ed325f4805448f06cb20dd36ee3d54a13e14
Antony <78171688+Antony-Ivanov@users.noreply.github.com>
2021-10-05 18:34:01 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,6 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2';
+const BASE_URL = 'https://restcountries.com/v3';
 
 function fetchCountries(searchQuery) {
   return fetch(`${BASE_URL}/name/${searchQuery}`).then(response => response.json());
 }
-
 export default { fetchCountries };
