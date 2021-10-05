5eabef1d4a41a9d65dbcd87b2c6a781bbc2dadc1
Antony <78171688+Antony-Ivanov@users.noreply.github.com>
2021-10-05 23:15:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+import './sass/main.scss';
+
+import refs from './js/refs';
+import API from './js/fetchCountries';
+import orderCountries from './templates/cardCountries';
+import templateCardCountries from './templates/templateCountries';
+
+import debounce from 'lodash.debounce';
+
+import { error, notice } from '../node_modules/@pnotify/core/dist/PNotify.js';
+
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+function searchInput(e) {
+  const searchQuery = e.target.value;
+  refs.countriesMarkup.innerHTML = '';
+
+  if (searchQuery.length < 1 && searchQuery === '') return;
+
+  API.fetchCountries(searchQuery).then(dataPreview).catch(noticeInfo);
+}
+
+function dataPreview(countries) {
+  if (countries.length > 10) {
+    error({
+      text: 'Too many matches found. Please enter a more specific query!',
+      delay: 5000,
+    });
+  }
+
+  if (countries.status === 404) {
+    errorMessage('Nothing was found for your query!');
+  }
+
+  if (countries.length > 1 && countries.length < 10) {
+    refs.countriesMarkup.innerHTML = orderCountries(countries);
+  }
+
+  if (countries.length === 1) {
+    refs.countriesMarkup.innerHTML = templateCardCountries(...countries);
+  }
+}
+console.log(countries)
+
+function errorMessage(message) {
+  error({
+    title: `${message}`,
+    delay: 2000,
+  });
+}
+
+const noticeInfo = () => {
+  notice({
+    text: 'Ivalid entered value',
+    delay: 2000,
+  });
+};
+
+refs.search.addEventListener('input', debounce(searchInput, 500));
